1329897a347eedd8c23dea3c5f2928235f90bf8c
Adrià <adriablancafort@gmail.com>
2026-04-19 20:29:04 +0200

    init react router

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router"
 
 import "@workspace/ui/globals.css"
-import { App } from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { AppRoutes } from "@/routes.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </ThemeProvider>
   </StrictMode>
 )
