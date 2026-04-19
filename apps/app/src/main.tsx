14e142a133c367e67cba6516f1da1ff620b7ec29
Adrià <adriablancafort@gmail.com>
2026-04-20 00:24:06 +0200

    init react query

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -1,17 +1,22 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import "@workspace/ui/globals.css"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
 import { AppRoutes } from "@/routes.tsx"
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>
 )
