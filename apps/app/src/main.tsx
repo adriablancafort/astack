61ef856a2c14a237253bf58f312ff0cea7683b16
Adrià <adriablancafort@gmail.com>
2026-04-20 02:14:12 +0200

    add sonner

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import "@workspace/ui/globals.css"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { Toaster } from "@workspace/ui/components/sonner"
 import { AppRoutes } from "@/routes.tsx"
 
 const queryClient = new QueryClient()
@@ -16,6 +17,7 @@ createRoot(document.getElementById("root")!).render(
         <BrowserRouter>
           <AppRoutes />
         </BrowserRouter>
+        <Toaster />
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>
