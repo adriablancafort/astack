ee1db639f4b3b8202d22947ec58829ab913fab35
Adrià <adriablancafort@gmail.com>
2026-04-20 12:29:51 +0200

    add tooltip

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import "@workspace/ui/globals.css"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { TooltipProvider } from "@workspace/ui/components/tooltip"
 import { Toaster } from "@workspace/ui/components/sonner"
 import { AppRoutes } from "@/routes.tsx"
 
@@ -14,10 +15,12 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
-        <Toaster />
+        <TooltipProvider>
+          <BrowserRouter>
+            <AppRoutes />
+          </BrowserRouter>
+          <Toaster />
+        </TooltipProvider>
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>
